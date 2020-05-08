074d32ab98c7092d34c0916707e8f4215ad280c6
Daniel <cphangwei@student.usm.my>
2020-05-08 21:08:51 +0800

    8/5/2020
    completed function 1: Strongly Connected

--- adjacencyMatrix.java
+++ adjacencyMatrix.java
@@ -5,7 +5,26 @@ import java.util.*;
 public class adjacencyMatrix {
     private int[][] incomingEdge = new int[5][5];
     private int[][] outgoingEdge = new int[5][5];
+    private int[][] dfsMatrix = new int[5][5]; //for DFS
     private Map<String,Integer> ref= new HashMap<String,Integer>();
+
+    public void initialize(){
+        ref.put("AU",0);
+        ref.put("BE",1);
+        ref.put("DK",2);
+        ref.put("EG",3);
+        ref.put("HK",4);
+
+        for(int[] i: incomingEdge){Arrays.fill(i,0);}
+        for(int[] i: outgoingEdge){Arrays.fill(i,0);}
+        for(int[] i: dfsMatrix){Arrays.fill(i,0);}
+
+        addEdge("AU","EG",12);
+        addEdge("AU","HK",6);
+        addEdge("DK","EG",4);
+        addEdge("DK","BE",1);
+        addEdge("HK","BE",9);
+    }
 
     public void addEdge(String origin, String destination, int weight){
         outgoingEdge[ref.get(origin)][ref.get(destination)] = weight;
@@ -26,21 +45,13 @@ public class adjacencyMatrix {
         }
     }
 
-    public void displayIncomingMatrix(){
-        System.out.println(Arrays.deepToString(incomingEdge));
-    }
-
+    public void displayIncomingMatrix(){System.out.println(Arrays.deepToString(incomingEdge));}
     public void displayOutgoingMatrix(){
         System.out.println(Arrays.deepToString(outgoingEdge));
     }
 
-    public boolean isOutgoing(String origin,String destination){
-        return outgoingEdge[ref.get(origin)][ref.get(destination)] != 0;
-    }
-
-    public boolean isIncoming(String origin,String destination){
-        return incomingEdge[ref.get(origin)][ref.get(destination)] != 0;
-    }
+    public boolean isOutgoing(String origin,String destination){return outgoingEdge[ref.get(origin)][ref.get(destination)] != 0;}
+    public boolean isIncoming(String origin,String destination){return incomingEdge[ref.get(origin)][ref.get(destination)] != 0;}
 
     public String getKeyByValue(Map<String,Integer> map, int index){
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
@@ -51,34 +62,80 @@ public class adjacencyMatrix {
         return null;
     }
 
-    public ArrayList<String> adjacent(String vertex){
+    public ArrayList<String> adjacentVertices(String vertex){
         int index = ref.get(vertex);
         ArrayList<String> adjacent = new ArrayList<String>();
         for(int i=0; i<5; ++i){
             if(outgoingEdge[index][i] != 0){
                 adjacent.add(getKeyByValue(ref,i));
             }
-            if(incomingEdge[index][i] != 0){
-                adjacent.add(getKeyByValue(ref,i));
-            }
         }
         return adjacent;
     }
 
-    public void initialize(){
-        ref.put("AU",0);
-        ref.put("BE",1);
-        ref.put("DK",2);
-        ref.put("EG",3);
-        ref.put("HK",4);
+    //Function 1: Determine if strongly connected. Will be using DFS
+    public void stronglyConnected(){
+        boolean stronglyConnect = false;
+        do {
+            depthFirstSearch();
+            stronglyConnect = dfsMatrixChecker();
+            if (stronglyConnect)
+                System.out.println("\nGraph is Strongly Connected!");
+            else {
+                System.out.println("\nGraph is NOT Strongly Connected!");
+                randomlyGenerateEdge();
+            }
+        }
+        while (!stronglyConnect);
+    }
 
-        for(int[] i: incomingEdge){Arrays.fill(i,0);}
-        for(int[] i: outgoingEdge){Arrays.fill(i,0);}
+    public void depthFirstSearch(){
+        String[] vertices = new String[] {"AU","BE","DK","EG","HK"};
 
-        addEdge("AU","EG",12);
-        addEdge("AU","HK",6);
-        addEdge("DK","EG",4);
-        addEdge("DK","BE",1);
-        addEdge("HK","BE",9);
+        for (int[] i:dfsMatrix)
+            Arrays.fill(i,0);
+
+        for(int x=0;x<5;++x){
+            System.out.print("\n\nFor starting vertex " + vertices[x]);
+            DFS(vertices[x],x);
+        }
+        System.out.print("\n" + Arrays.deepToString(dfsMatrix));
+    }
+
+    public void DFS(String vertex,int vertexArrayPosition){
+        int index = ref.get(vertex);
+        dfsMatrix[vertexArrayPosition][index] = 1;
+        ArrayList<String> vertexIsAdjacentTo = adjacentVertices(vertex);
+        System.out.print("\n" + vertex + " is adjacent to: " + vertexIsAdjacentTo);
+        for(String i:vertexIsAdjacentTo){
+            if (dfsMatrix[vertexArrayPosition][ref.get(i)]==0)
+                DFS(i,vertexArrayPosition);
+        }
+    }
+
+    public void randomlyGenerateEdge(){
+        Random random = new Random();
+        int source = random.nextInt(5);
+        int destination = random.nextInt(5);
+        System.out.println("\nSource: " + source);
+        System.out.println("Destination: " + destination);
+
+        while(source==destination||outgoingEdge[source][destination]!=0){
+             source = random.nextInt(5);
+             System.out.println("\nNew source: " + source);
+             destination = random.nextInt(5);
+            System.out.println("New destination: " + destination);
+        }
+        outgoingEdge[source][destination]=1;
+        System.out.println("An edge is generated between " + getKeyByValue(ref,source) + " and " + getKeyByValue(ref,destination));
+    }
+
+    public boolean dfsMatrixChecker(){
+        for (int[] matrix : dfsMatrix)
+            for (int i : matrix) {
+                if (i == 0)
+                    return false;
+            }
+        return true;
     }
 }
